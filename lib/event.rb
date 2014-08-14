cf2784a51a7393a5fbd9969e2b49649813e5fabf
Dylan Peerenboom & Ellen Krause Lehman <student@epicodus.com>
2014-08-14 16:51:48 -0700

    list events for tomorrow

--- lib/event.rb
+++ lib/event.rb
@@ -14,4 +14,14 @@ class Event < ActiveRecord::Base
     end
     upcomming
   end
+  def self.upcomming_day_events
+    now = Time.new
+    upcomming_day = []
+    self.upcomming_events.each do |event|
+      if event.start_date <= now + 1.day
+        upcomming_day << event
+      end
+    end
+    upcomming_day
+  end
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -6,6 +6,12 @@ describe Event do
     event2 = Event.create({:start_date => '2014-02-19 10:20:00', :end_date =>'2014-02-19 11:20:00', :location =>"Back Yard", :name => "BBQ"})
     event3 = Event.create({:start_date => '2014-09-19 10:20:00', :end_date =>'2014-09-19 11:20:00', :location =>"Back Yard", :name => "BBQ"})
     expect(Event.upcomming_events).to eq [event3, event]
+  end
 
+  it 'displays events tomorrow' do
+    event = Event.create({:start_date => '2014-08-15 02:20:00 -0700', :end_date =>'2014-10-19 11:20:00', :location =>"Back Yard", :name => "BBQ"})
+    event2 = Event.create({:start_date => '2014-08-14 22:20:00 -0700', :end_date =>'2014-08-19 11:20:00', :location =>"Back Yard", :name => "BBQ"})
+    event3 = Event.create({:start_date => '2014-09-19 10:20:00', :end_date =>'2014-09-19 11:20:00', :location =>"Back Yard", :name => "BBQ"})
+    expect(Event.upcomming_day_events).to eq [event2, event]
   end
 end
